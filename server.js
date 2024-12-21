fa0c74ed4d2f8392dfa24fe3901776500be7ed48
Christian Perlada <74958847+ampolperlada@users.noreply.github.com>
2024-12-21 11:47:13 +0800

    crud-1

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.get('/api/contacts', (req,res) => {
-  res.send('get all contacts')
-})
+app.use("/api/contacts", require("./routes/contactRoutes"));
+
 app.listen(port, () =>{
   console.log(`Server running on port ${port}`);
-})
+});
