1da21b8f8ef87d4b43ccd40379609a9d179679e7
Christian Perlada <74958847+ampolperlada@users.noreply.github.com>
2024-12-24 19:32:29 +0800

    exporting-2

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -6,7 +6,11 @@ const {getContacts} = require ('../controllers/contactController');
 
 const {createContacts} = require ('../controllers/contactController');
 
-const {createContacts} = require ('../controllers/contactController');
+const {getContact} = require ('../controllers/contactController');
+
+const {updateContact} = require ('../controllers/contactController');
+
+const {deleteContact} = require ('../controllers/contactController');
 
 
 
