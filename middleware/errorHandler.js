ad2ed3abf4120a833a6d283e2e28c36edc595d79
Christian Perlada <74958847+ampolperlada@users.noreply.github.com>
2025-01-04 19:26:31 +0800

    switchcasetofix

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,8 +1,28 @@
 const errorHandler = (err, req, res, next) =>{
 
   const statusCode = res.statusCode ? res.statusCode : 500;
-  res.json({ title:"Not Found ", message: err.message, stackTrace: err.stack});
-  res.json({ title:"Validation Failed ", message: err.message, stackTrace: err.stack});
+  switch (statusCode) {
+    case 400:
+      res.json
+      ({ 
+        title:"Validation Failed ",
+         message: err.message, 
+         stackTrace: err.stack
+      });
+
+      break;
+
+      case 404:
+        res.json
+        ({ title:"Not Found ", 
+          message: err.message, 
+          stackTrace: err.stack
+        });
+
+  
+    default:
+      break;
+  }
 
 };
 
