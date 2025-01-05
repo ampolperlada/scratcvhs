2db0813168fff0d3e70c3469c544933569417e24
Christian Perlada <74958847+ampolperlada@users.noreply.github.com>
2025-01-05 20:44:54 +0800

    complete-errorhandlers

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -23,21 +23,30 @@ const errorHandler = (err, req, res, next) =>{
 
         case constants.UNAUTHORIZED:
           res.json
-          ({ title:"Not Found ", 
+          ({ title:"Unauthorized ", 
             message: err.message, 
             stackTrace: err.stack
           });
 
           case constants.FORBIDDEN:
             res.json
-            ({ title:"Not Found ", 
+            ({ title:"Forbidden ", 
               message: err.message, 
               stackTrace: err.stack
             });
+
+            case constants.SERVER_ERROR:
+              res.json
+              ({ title:"Server Error ", 
+                message: err.message, 
+                stackTrace: err.stack
+              });
+      
     
 
   
     default:
+      console.log("No Error All is Well ");
       break;
   }
 
