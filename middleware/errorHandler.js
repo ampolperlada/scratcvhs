8f5152be0f122a514dd29536a35146b42757c679
Christian Perlada <74958847+ampolperlada@users.noreply.github.com>
2025-01-05 20:26:15 +0800

    importingerrhandlers-1

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -4,7 +4,7 @@ const errorHandler = (err, req, res, next) =>{
 
   const statusCode = res.statusCode ? res.statusCode : 500;
   switch (statusCode) {
-    case 400:
+    case constants.VALIDATION_ERROR:
       res.json
       ({ 
         title:"Validation Failed ",
@@ -14,12 +14,27 @@ const errorHandler = (err, req, res, next) =>{
 
       break;
 
-      case 404:
+      case constants.NOT_FOUND:
         res.json
         ({ title:"Not Found ", 
           message: err.message, 
           stackTrace: err.stack
         });
+
+        case constants.UNAUTHORIZED:
+          res.json
+          ({ title:"Not Found ", 
+            message: err.message, 
+            stackTrace: err.stack
+          });
+
+          case constants.FORBIDDEN:
+            res.json
+            ({ title:"Not Found ", 
+              message: err.message, 
+              stackTrace: err.stack
+            });
+    
 
   
     default:
