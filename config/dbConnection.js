f69c0a4a55e60e9a03089f167a92bdddffa6d594
Christian Perlada <74958847+ampolperlada@users.noreply.github.com>
2025-01-06 16:16:03 +0800

    try-catch-error-1

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,1 +1,11 @@
 const mongoose = require('mongoose');
+
+const connectDb = async () => {
+  try{
+  }catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+}
+
+module.exports = connectDb;
