a628d4b6bb194ed5b3c55d18d106aae17ed09941
Christian Perlada <74958847+ampolperlada@users.noreply.github.com>
2025-01-09 21:54:43 +0800

    complete-schema

--- models/contactModel.js
+++ models/contactModel.js
@@ -6,6 +6,17 @@ const contactSchema = mongoose.Schema({
     required: [true, "Please add the contact name"],
    },
    email: {
-    
+    type: String, 
+    required: [true, "Please add the contact Email Address"],
+   },
+   email: {
+    type: String, 
+    required: [true, "Please add the contact Phone Number"],
    }
-});
+},{
+  timestamps: true,
+}
+
+);
+
+module.exports = mongoose.model("Contact", contactSchema);
