fe46057c256c6fef058d0e187c98b9943eee5c77
Christian Perlada <74958847+ampolperlada@users.noreply.github.com>
2025-01-09 20:49:15 +0800

    adding-schemas

---
+++ models/contactModel.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const contactSchema = mongoose.Schema({
+   name: {
+    type: String, 
+    required: [true, "Please add the contact name"],
+   },
+   email: {
+    
+   }
+});
